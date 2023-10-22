a2794148ea6e3ca0702c993aa6a8f8130841d375
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2023-10-22 13:11:16 +0300

    Replace tempfile crate with ggstd

--- tests/profile.rs
+++ tests/profile.rs
@@ -1,10 +1,11 @@
 use cbindgen::*;
 
+use ggstd::os::TempDir;
 use serial_test::serial;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-fn build_using_lib(config: fn(Builder) -> Builder) -> tempfile::TempDir {
+fn build_using_lib(config: fn(Builder) -> Builder) -> TempDir {
     let expand_dep_test_dir = {
         let mut this_file = PathBuf::from(file!());
         this_file.pop();
@@ -12,12 +13,9 @@ fn build_using_lib(config: fn(Builder) -> Builder) -> tempfile::TempDir {
         this_file
     };
 
-    let tmp_dir = tempfile::Builder::new()
-        .prefix("cbindgen-test-output-")
-        .tempdir()
-        .expect("Creating tmp dir failed");
+    let tmp_dir = TempDir::new("cbindgen-test-output-").expect("Creating tmp dir failed");
 
-    std::env::set_var("CARGO_EXPAND_TARGET_DIR", tmp_dir.path());
+    std::env::set_var("CARGO_EXPAND_TARGET_DIR", &tmp_dir.path);
     let builder = Builder::new()
         .with_config(Config::from_file(expand_dep_test_dir.join("cbindgen.toml")).unwrap())
         .with_crate(expand_dep_test_dir);
@@ -27,7 +25,7 @@ fn build_using_lib(config: fn(Builder) -> Builder) -> tempfile::TempDir {
     tmp_dir
 }
 
-fn build_using_bin(extra_args: &[&str]) -> tempfile::TempDir {
+fn build_using_bin(extra_args: &[&str]) -> TempDir {
     let expand_dep_test_dir = {
         let mut this_file = PathBuf::from(file!());
         this_file.pop();
@@ -35,16 +33,13 @@ fn build_using_bin(extra_args: &[&str]) -> tempfile::TempDir {
         this_file
     };
 
-    let tmp_dir = tempfile::Builder::new()
-        .prefix("cbindgen-test-output-")
-        .tempdir()
-        .expect("Creating tmp dir failed");
+    let tmp_dir = TempDir::new("cbindgen-test-output-").expect("Creating tmp dir failed");
 
     let cbindgen_path = env!("CARGO_BIN_EXE_cbindgen");
 
     Command::new(cbindgen_path)
         .current_dir(expand_dep_test_dir)
-        .env("CARGO_EXPAND_TARGET_DIR", tmp_dir.path())
+        .env("CARGO_EXPAND_TARGET_DIR", &tmp_dir.path)
         .args(extra_args)
         .output()
         .expect("build should succeed");
@@ -64,37 +59,37 @@ fn get_contents_of_dir(path: &Path) -> Vec<String> {
 #[serial]
 fn lib_default_uses_debug_build() {
     let target_dir = build_using_lib(|b| b);
-    assert_eq!(get_contents_of_dir(target_dir.path()), &["debug"]);
+    assert_eq!(get_contents_of_dir(&target_dir.path), &["debug"]);
 }
 
 #[test]
 #[serial]
 fn lib_explicit_debug_build() {
     let target_dir = build_using_lib(|b| b.with_parse_expand_profile(Profile::Debug));
-    assert_eq!(get_contents_of_dir(target_dir.path()), &["debug"]);
+    assert_eq!(get_contents_of_dir(&target_dir.path), &["debug"]);
 }
 
 #[test]
 #[serial]
 fn lib_explicit_release_build() {
     let target_dir = build_using_lib(|b| b.with_parse_expand_profile(Profile::Release));
-    assert_eq!(get_contents_of_dir(target_dir.path()), &["release"]);
+    assert_eq!(get_contents_of_dir(&target_dir.path), &["release"]);
 }
 
 #[test]
 fn bin_default_uses_debug_build() {
     let target_dir = build_using_bin(&[]);
-    assert_eq!(get_contents_of_dir(target_dir.path()), &["debug"]);
+    assert_eq!(get_contents_of_dir(&target_dir.path), &["debug"]);
 }
 
 #[test]
 fn bin_explicit_debug_build() {
     let target_dir = build_using_bin(&["--profile", "debug"]);
-    assert_eq!(get_contents_of_dir(target_dir.path()), &["debug"]);
+    assert_eq!(get_contents_of_dir(&target_dir.path), &["debug"]);
 }
 
 #[test]
 fn bin_explicit_release_build() {
     let target_dir = build_using_bin(&["--profile", "release"]);
-    assert_eq!(get_contents_of_dir(target_dir.path()), &["release"]);
+    assert_eq!(get_contents_of_dir(&target_dir.path), &["release"]);
 }
